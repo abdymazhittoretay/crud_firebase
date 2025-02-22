b3215e7076a304e2e706c3d2cf6ad43f6dee5fe6
Toretay <abdymvzhit@gmail.com>
2025-02-22 23:52:06 +0500

    connected firebase with the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:crud_firebase/firebase_options.dart';
 import 'package:crud_firebase/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
