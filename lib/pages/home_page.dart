4730ccf8d12faf190f500dea301ec8cb5d9cce03
Toretay <abdymvzhit@gmail.com>
2025-02-25 17:00:35 +0500

    added streamBuilder with readNotes function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crud_firebase/services/firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +10,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirestoreService firestoreService = FirestoreService();
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +22,35 @@ class _HomePageState extends State<HomePage> {
         surfaceTintColor: Colors.black,
         title: Text("CRUD Firebase"),
         centerTitle: true,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestoreService.readNotes(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+            List notes = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot document = notes[index];
+                final String docID = document.id;
+                final Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                final String note = data["note"];
+                return ListTile(
+                  title: Text(note),
+                  trailing: IconButton(
+                    onPressed: () {
+                      firestoreService.deleteNote(docID);
+                    },
+                    icon: Icon(Icons.delete),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text("There are no notes yet."));
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.black,
@@ -36,16 +69,19 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.white,
           shape: LinearBorder(),
           title: Text("Add some data:"),
-          content: TextField(),
+          content: TextField(controller: _controller),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
+                _controller.clear();
               },
               child: Text("Cancel"),
             ),
             TextButton(
               onPressed: () {
+                firestoreService.addNote(_controller.text);
+                _controller.clear();
                 Navigator.pop(context);
               },
               child: Text("Add"),
